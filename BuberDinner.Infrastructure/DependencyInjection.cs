8d5a677273775b2c3ba9cc7a8fef8f61ddc72c91
Dimitris Tsounis <dimitris_tsounis@windowslive.com>
2022-06-18 23:39:37 +0300

    added-seperate-dependency-inj-foreach-project

---
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Infrastructure;
+
+public static class DependendyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
