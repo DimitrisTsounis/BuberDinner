ca739f6c4949ce552715ec024e562d4bcf09c863
Dimitris Tsounis <dimitris_tsounis@windowslive.com>
2022-06-19 17:08:15 +0300

    added-jwt-token-generation

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,15 +1,33 @@
+using BuberDinner.Application.Common.interfaces.Authentication;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwTokenGenerator)
+    {
+        _jwTokenGenerator = jwTokenGenerator;
+    }
+
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
+        // Check if user already exists
+
+        // Create user (generate unique ID)
+
+        // Create JWT token
+        Guid userId = Guid.NewGuid();
+
+        var token = _jwTokenGenerator.GenerateToken(userId, firstName, lastName);
+
         return new AuthenticationResult(
-            Guid.NewGuid(),
+            userId,
             firstName,
             lastName,
             email,
-            "token");
+            token);
     }
 
     public AuthenticationResult Login(string email, string password)
